a354436cdaa97cbf43eb3a472c5e164b8430c9fd
lqie02 <118864814+lqie02@users.noreply.github.com>
2022-11-23 12:24:57 +0800

    Update regStaff.php

--- regStaff.php
+++ regStaff.php
@@ -19,7 +19,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 	$staffType = test_input($_POST['staffType']);
 	$password = test_input($_POST['password']);
 	$cpassword =test_input($_POST['cpassword']);
-	$department_id =$_POST['department_id'];
+	$department_id =test_input($_POST['department_id']);
 
 
 	if($password != $cpassword)
@@ -31,11 +31,12 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
 		
 		//$qry="SELECT d.department_id FROM department d join staff s ON s.department_id= d.department_id WHERE staffType=$staffType";
+		//$result = $mysqli -> query($qry);
+		//$row = $result -> fetch_assoc();
 
-
-			
-
-
+		//$result = mysqli_query($conn, $query);
+		//$row = mysqli_fetch_assoc($result);
+		
 		$query = "INSERT INTO staff(staffName, staffEmail, staffType, staffPassword,manager_id,department_id) VALUES ('$staffName', '$staffEmail', '$staffType', '$password', 11000, '$department_id')";
 
 		
@@ -104,11 +105,27 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 			<select class="form-control" name="staffType" value="<?php echo $staffType; ?>" required>
                     	 <option selected="true" disabled="disabled" value="">- Select role -</option >
                      	 <option value="rider">Rider</option>
-                     	 <option value="hrStaff">HR</option>
-                     	 <option value="kitchenStaff">Kitchen</option>
-                     	 <option value="FloorStaff">Floor</option>
+                     	 <option value="hr Staff">HR Staff</option>
+                     	 <option value="kitchen Staff">Kitchen Staff</option>
+                     	 <option value="Floor Staff">Floor Staff</option>
             </select> 
         </div></br>
+		
+		
+	
+			 <div calss = "form-group">
+				<label class = "col-sm-3 control-label"> &nbsp;Select Department&nbsp;&nbsp;</label> 
+			<select class="form-control" name="department_id" value="<?php echo $department_id; ?>" required>
+                    	 <option selected="true" disabled="disabled" value="">- Select department -</option >
+                     	 <option value=8003>Rider</option>
+                     	 <option value=8004>HR</option>
+                     	 <option value=8002>Kitchen</option>
+                     	 <option value=8001>Floor</option>
+				
+				
+				
+            </select> 
+        </div></br> 
 
 			<div class="input-group">
 				<button name="submit" class="btn">Register</button>
