1a4919127e732ade41cbdbd5d34af78fc7b07c98
AisyAtiqah <85858867+AisyAtiqah@users.noreply.github.com>
2022-12-04 16:28:15 +0800

    Add files via upload

--- admin/js/script.js
+++ admin/js/script.js
@@ -14,5 +14,5 @@ window.onscroll = () =>{
 
 document.querySelector('#close-edit').onclick = () =>{
    document.querySelector('.edit-form-container').style.display = 'none';
-   window.location.href = 'admin.php';
+   window.location.href = 'addproduct.php';
 };
