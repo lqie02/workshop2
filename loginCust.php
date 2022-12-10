091ac4b87fd8353d0c74678da4cd9144c5ccc475
lqie02 <118864814+lqie02@users.noreply.github.com>
2022-12-11 02:42:37 +0800

    Update loginCust.php

--- loginCust.php
+++ loginCust.php
@@ -28,16 +28,19 @@ if(isset($_POST['btn_login']))
         $row = $result->fetch_assoc();
 
         $_SESSION['customer_id'] = $row["customer_id"];
-        $_SESSION['custname'] = $row['custName'];
+        $_SESSION['custName'] = $row['custName'];
+		$_SESSION['custEmail'] = $row['custEmail'];
+		$_SESSION['custTel'] = $row['custTel'];
+		$_SESSION['address'] = $row['address'];
 
-       
+       // mysqli_query($conn,"delete from loginlogs where IpAddress='$ip_address'");
         echo "<script>alert('Login Success!');</script>";
         echo"<meta http-equiv='refresh' content='0; url=http://localhost/fkfood/Location.php />";
 		  
       }
       else
         echo "<script>alert('Woops! Email or Password was wrong');</script>";
-        echo"<meta http-equiv='refresh' content='0; url=loginCust.php'>";
+        echo"<meta http-equiv='refresh' content='0; url=loginCust.php'/>";
 
 }
 ?>
@@ -47,44 +50,42 @@ if(isset($_POST['btn_login']))
 <head>
 
 	<meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="img/2.png" type="image/png" sizes="20x20">
 	
     <!-- Bootstrap core CSS-->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Custom styles for this template-->
     <link rel="stylesheet" type="text/css" href="style1.css">	
-
+	<title>Customer Login</title>
 </head>
 <body>
 
-<section class="h-100 gradient-form">
-
-  <form name="loginForm" method="post" >
+	<div class="container">
     <form action="" method="POST" class="login-email">
-      <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
-      <div class="container">
-        <p>Please login to your account</p>
+      <p align="center" class="login-text" style="font-size: 2rem; font-weight: 800;">Login Customer</p>
+      <br>
+        <p align="justify">Please login to your account : </p><br>
 
         <div class="input-group">
-          <input type="email" name="email" id="form2Example11" class="form-control" placeholder="Email address" autofocus required/>
+          <input type="email" name="email" placeholder="Email address" autofocus required/>
         </div>
 
         <div class="input-group">
-          <input type="password" name="password" id="form2Example22" minlength="8" class="form-control" placeholder="Password" autofocus required/>
-        </div>
+          <input type="password" name="password" minlength="8"  placeholder="Password" autofocus required/>
+        </div><br>
 
 
-          <div class="text-center pt-3 mb-5 pb-1">
-          <a href="location.php"></a><button name="btn_login" class="btn">Log in></button>
+
+          <div class="input-group">
+          <button name="btn_login" class="btn">Log in</button>
           </div>
 
           <div class="d-flex align-items-center justify-content-center pb-4">
-          <p class="login-register-text">Don't have an account?
-          <a href="#" onclick="window.open('http://localhost/fkfood/regCust.php')"; >Register Here</a>.</p>
+          <p align="center" class="login-register-text">Do not have an account?
+          <a href="regCust.php" >Register Here</a>.</p>
           </div>
 
           <div class="result text-center mb-0 text-danger"  id="result"><p><?php echo $msg?></p>
