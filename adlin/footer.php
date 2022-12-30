4308610ec7e427e1c92172a3c50197c5c949c94e
AisyAtiqah <85858867+AisyAtiqah@users.noreply.github.com>
2022-12-31 00:59:46 +0800

    Update footer.php

--- adlin/footer.php
+++ adlin/footer.php
@@ -1,3 +1,54 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+	</style>
+</head>
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" a href="footer.css">  
+ <footer class="footer-distributed">
 
+      <div class="footer-left">
+          <center><img src="2.png"></center>
+      
 
+        <p class="footer-links">
+          <a href="">Home</a>
+          |
+          <a href="">Menu</a>
+          
+      <!--<a href="">About</a>
+          |
+          <a href="">Contact</a> -->
+        </p>
+
+        <p class="footer-company-name">COPYRIGHT © 2022 ALL RIGHTS RESERVED BY FK RESTAURANT</p>
+      </div>
+
+      <div class="footer-center">
+        <div>
+
+            <p>Sokongan</p>
+            <br></br>
+            Terma & Syarat</br>
+			 <br>Dasar Pemesanan Web</br>
+			 <br>Polisi Privasi dan Perlindungan Data Peribadi</br>
+			 <br>Maklumat Pemakanan</br>
+			 <br>Soalan Lazim</br>
+			 <br>Mobile Web</br>
+     </div>
+ </div>
+      <div class="footer-right">
+        <p class="footer-company-about">
+          <p>Ikuti Kami</p>
+          <p><a href=""><img src="../img/fb.png"   height="50" width="50"></a></p>
+        </div>
+      </div>
+    </footer>
+</body>
 </html>
