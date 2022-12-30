5027a84b5f36bbc6f8a212c24595c85552b2e330
AisyAtiqah <85858867+AisyAtiqah@users.noreply.github.com>
2022-12-31 01:01:47 +0800

    Update header.php

--- adlin/customer/header.php
+++ adlin/customer/header.php
@@ -36,9 +36,9 @@
    <div class="menu-bar">
       <h1 class="logo">FK<span>Restaurant</span></h1>
       <ul>     
+         <li><a href="homepage.php">Home  <i class="fa fa-home"></i></a></li>
        <li><a href="viewmenu.php">Menu<i class="fas fa-drumstick-bite"></i></a></li>
         <li><a href="category.php">Category <i class="fa fa-list"></i></a></li>
-        <li><a href="Location.php">Locate us <i class="fas fa-map-marker-alt"></i></a></li>
         <li><a href="edit.php">Edit Profile <i class="fa fa-user"></i></a></li>
         <li><a href="contact.php">Contact us  <i class="fa fa-phone"></i></a></li>
         <li><a href="cart.php">View Cart <i class="fa fa-cart-plus"></i></a></li>
