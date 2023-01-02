e8ccdb91866fd9a47b9bc084cfd90718e02723bf
AisyAtiqah <85858867+AisyAtiqah@users.noreply.github.com>
2023-01-02 19:04:00 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php include('includes/DatabaseClass.php'); ?>
+<?php $db = new DatabaseClass ();  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="img/2.png" type="image/png" sizes="20x20">
+	
+    <!-- Bootstrap core CSS-->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template-->
+    <link rel="stylesheet" type="text/css" href="style1.css">	
+  	<title><?php echo  $title; ?></title>
+</head>
