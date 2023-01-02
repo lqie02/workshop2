51336549586c99f10d8ed5011c0dea708d344864
AisyAtiqah <85858867+AisyAtiqah@users.noreply.github.com>
2023-01-02 18:39:00 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,54 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+	</style>
+</head>
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" a href="footer.css">  
+ <footer class="footer-distributed">
+
+      <div class="footer-left">
+          <center><img src="img/2.png"></center>
+      
+
+        <p class="footer-links">
+          <a href="">Home</a>
+          |
+          <a href="">Menu</a>
+          
+      <!--<a href="">About</a>
+          |
+          <a href="">Contact</a> -->
+        </p>
+
+        <p class="footer-company-name">COPYRIGHT © 2022 ALL RIGHTS RESERVED BY FK RESTAURANT</p>
+      </div>
+
+      <div class="footer-center">
+        <div>
+
+            <p>Support</p>
+            <br></br>
+            Terms & Conditions</br>
+			 <br>Web Ordering Policy</br>
+			 <br>Privacy Policy</br>
+			 <br>FAQ</br>
+			 <br>Nutritional Information</br>
+			 <br>Mobile Web</br>
+     </div>
+ </div>
+      <div class="footer-right">
+        <p class="footer-company-about">
+          <p>Follow Us</p>
+          <p><a href=""><img src="../img/fb.png"   height="50" width="50"></a></p>
+        </div>
+      </div>
+    </footer>
+</body>
+</html>
